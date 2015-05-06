da27ad9698cb69ae22a59626da13ab2fa0f50987
chen-qilin <chen_qilin@163.com>
2015-05-06 22:30:59 +0800

    Update and rename network/send_email.py to Network/send_email.py

---
+++ Network/send_email.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding = utf-8
+
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+
+sender = 'chen_qilin@163.com'
+receiver = 'chen_qilin@163.com'
+subject = 'python email test'
+username = 'chen_qilin'
+password = 'chen@1101'
+
+msg = MIMEText('Hello World', 'plain')
+msg['Subject'] = Header(subject)
+
+smtp = smtplib.SMTP()
+smtp.connect('smtp.163.com')
+smtp.login(username, password)
+smtp.sendmail(sender, receiver, msg.as_string())
+smtp.quit()
