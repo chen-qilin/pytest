89f45592a26fddc4dbd027fd326e4e6d55bc01f4
chen-qilin <chen_qilin@163.com>
2016-04-19 23:20:19 +0800

    Update portscanner.py

--- Hacker/portscanner.py
+++ Hacker/portscanner.py
@@ -3,6 +3,9 @@
 
 import optparse
 from socket import *
+from threading import *
+
+screen_lock = Semaphore(value=1)
 
 
 def conn_scan(tgthost, tgtport):
@@ -11,31 +14,34 @@ def conn_scan(tgthost, tgtport):
         conn_skt.connect((tgthost, tgtport))
         conn_skt.send(b'ViolentPython\r\n')
         results = conn_skt.recv(1024)
+        screen_lock.acquire()
         print('[+] %d/tcp open' % tgtport)
         print('[+] ' + str(results))
         conn_skt.close()
     except Exception:
+        screen_lock.acquire()
         print('[-] %d/tcp closed' % tgtport)
+    finally:
+        screen_lock.release()
+        conn_skt.close()
 
 
 def port_scan(tgthost, tgtports):
     try:
         tgt_ip = gethostbyname(tgthost)
-        print(1)
     except Exception:
         print("[-] Cannot resolve '%s': Unknown host" % tgthost)
         return None
     try:
         tgtname = gethostbyaddr(tgt_ip)
-        print(2)
         print('\n[+] Scan Results for: ' + tgtname[0])
     except Exception:
-        print(3)
         print('\n[+] Scan Results for:' + tgt_ip)
     setdefaulttimeout(20)
     for tgtport in tgtports:
         print('Scanning port ' + tgtport)
-        conn_scan(tgthost, int(tgtport))
+        t = Thread(target=conn_scan, args=(tgthost, int(tgtport)))
+        t.start()
 
 
 def main():
@@ -44,11 +50,11 @@ def main():
     parser.add_option('-p', dest='tgtport', type='string', help='specify target port[s] separated by comma')
     (options, args) = parser.parse_args()
     tgthost = options.tgthost
-    tgtport = str(options.tgtport).split(',')
-    if (tgthost is None) | (tgtport == ['None']):
+    tgtports = str(options.tgtport).split(',')
+    if (tgthost is None) or (tgtports == ['None']):
         print('[-] You must specify a target host and port[s].')
         exit(0)
-    port_scan(tgthost, tgtport)
+    port_scan(tgthost, tgtports)
 
 if __name__ == '__main__':
     main()
